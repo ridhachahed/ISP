603954d263634f66841a337bb302f586672b7ea2
ridha <ridha.chahed@gmail.com>
2020-10-25 11:42:02 +0100

    Not working ex4

--- HW2/Ex4/client.py
+++ HW2/Ex4/client.py
@@ -5,63 +5,87 @@ import os
 
 EMAIL = "mohamed.chahed@epfl.ch"
 PASSWORD = "correct horse battery staple"
-#H = sha256 ??
 N = "EEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496EA81D3383B4813D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8EF4AD69B15D4982559B297BCF1885C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA9AFD5138FE8376435B9FC61D2FC0EB06E3"
 g = 2
 size = 32
 
-async def pake(websocket, path):
 
-    email_utf8_hex = format(EMAIL, "x").encode()
-    await websocket.send(email_utf8_hex)
-    #hash.update(utf8_hex)
+async def pake():
+    async with websockets.connect(
+            'ws://127.0.0.1:5000') as websocket:
 
-    # Number is received as UTF-8 encoded hexadecimal String
-    salt_utf8_hex = await websocket.recv()
+        # Send email UTF-8 encoded
+        email_utf8 = EMAIL.encode()
+        await websocket.send(email_utf8)
+        print("Email sent : " + EMAIL)
 
-    # Hexadecimal string is converted back to an integer
-    salt_integer = int(salt_utf8_hex, 16)
+        # Salt is received as UTF-8 encoded hexadecimal String
+        salt_utf8_hex = await websocket.recv()
 
-    random_bytes = os.urandom(size)
-    a = int.from_bytes(random_bytes)
-    n_int = int(N, 16)
-    A = pow(g, a, n_int)
+        # Hexadecimal string is converted back to an integer
+        salt_integer = int(salt_utf8_hex, 16)
 
-    b_utf8_hex = await websocket.recv()
+        print("Salt received:" + str(salt_integer))
 
-    # u = H(A || B)
-    u = hashlib.sha256()
-    u.update(format(A, "x").encode())
-    u.update(b_utf8_hex)
-    # digest() returns an hexadecimal representation of the hash
-    U = u.digest()
+        # generate 32 random bytes
+        random_bytes = os.urandom(size)
+        # generate integer form bytes
+        a = int.from_bytes(random_bytes, 'big')
+        n_int = int(N, 16)
+        A = pow(g, a, n_int)
 
-    # x = H(salt || H(U || ":" || PASSWORD))
-    x = hashlib.sha256()
-    x.update(salt_utf8_hex)
+        print("A computed : " + str(A))
 
-    # y = H(U || ":" || PASSWORD)
-    # x = H(salt || Y)
-    y = hashlib.sha256()
-    y.update(U.encode())
-    y.update(format(':', "x").encode())
-    y.update(format(PASSWORD, "x").encode())
-    Y = y.digest()
+        # send A as UTF-8 encoded hexadecimal String to server
+        a_utf8_hex = format(A, "x").encode()
 
-    x.update(Y.encode())
+        await websocket.send(a_utf8_hex)
 
-    # doubts on encode here
-    x_integer = int(x.digest().encode(), 16)
+        # B is received as UTF-8 encoded hexadecimal String
+        b_utf8_hex = await websocket.recv()
 
-    # S = (B - g ^ x) ^ (a + u * x) % N
-    B = int(b_utf8_hex,16)
+        print(b_utf8_hex)
 
-    S = pow(B - g ** x_integer, a + u * x_integer)
+        # u = H(A || B)
+        u = hashlib.sha256()
+        u.update(a_utf8_hex)
+        u.update(b_utf8_hex.encode())
 
-    await websocket.send(S)
+        # hexdigest() returns an hexadecimal representation of the hash
+        u_integer = int(u.hexdigest(), 16)
 
-start_server = websockets.serve(pake, "ws://127.0.0.1:/", 5000)
+        # x = H(salt || H(U || ":" || PASSWORD))
+        x = hashlib.sha256()
+        x.update(salt_utf8_hex.encode())
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+        # y = H(U || ":" || PASSWORD)
+        # x = H(salt || Y)
+        y = hashlib.sha256()
+        y.update(EMAIL.encode())
+        y.update(':'.encode())
+        y.update(PASSWORD.encode())
+        Y = y.hexdigest()
 
+        x.update(Y.encode())
+
+        x_integer = int(x.hexdigest(), 16)
+
+        # S = (B - g ^ x) ^ (a + u * x) % N
+        b_integer = int(b_utf8_hex, 16)
+
+        S = pow(b_integer - g ** x_integer, a + u_integer * x_integer, n_int)
+
+        print('S obtained :' +str(S))
+
+        #send H(A || B || S)
+        final_hash = hashlib.sha256()
+        final_hash.update(a_utf8_hex)
+        final_hash.update(b_utf8_hex.encode())
+        final_hash.update(format(S, "x").encode())
+        await websocket.send(final_hash)
+
+        # get token
+        token = await websocket.recv()
+        print(token)
+
+asyncio.get_event_loop().run_until_complete(pake())
